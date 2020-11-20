5542b2b1943c856c944c9765a75157168afa8f13
seunghan.kim <seunghan.kim@tablenote.co>
2020-11-20 17:02:20 +0900

    iOS 12에서 검색 결과 화면이 전체를 감싸는 문제 수정

--- HKImageDemo/HKImageDemo/Unsplash/PhotoList/Search/PhotoSearchListViewController.swift
+++ HKImageDemo/HKImageDemo/Unsplash/PhotoList/Search/PhotoSearchListViewController.swift
@@ -10,5 +10,8 @@ import Foundation
 class PhotoSearchListViewController: PhotoListViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // iOS 12.X 에서 검색창을 덮어버리는 문제 수정
+        self.edgesForExtendedLayout = []
     }
 }
